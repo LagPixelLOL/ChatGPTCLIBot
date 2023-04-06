28f0fceb94d0b0d9cf844dfb87fac134c7f673a4
v2ray <2282688304@qq.com>
2023-04-07 04:23:35 +0800

    Added logit_bias parameter to the config.

--- main/interface/network/Network.h
+++ main/interface/network/Network.h
@@ -17,6 +17,7 @@ namespace api {
     bool call_api(const string& initial_prompt, const vector<std::shared_ptr<chat::Exchange>>& chat_exchanges,
                   const string& api_key, const string& model, const float& temperature, const int& max_tokens,
                   const float& top_p, const float& frequency_penalty, const float& presence_penalty,
+                  const unordered_map<uint16_t, float>& logit_bias,
                   const unsigned int& max_short_memory_length, const unsigned int& max_reference_length,
                   const string& me_id, const string& bot_id, std::function<void(const string& streamed_response)> callback,
                   const bool& debug_reference = false, const bool& pause_when_showing_reference = true);
